0b93c1f4dcf309c73cd0d189c93d6548fac20ca0
TonyZ1z <1104606022@qq.com>
2018-11-21 00:03:25 +0800

    add other method to use the setState()

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -13,8 +13,9 @@ class Header extends Component{
     }
 
     render(){
+        const root = "root2"
         return (
-            <div onClick={this.handleClickOnHeader}>
+            <div id={root} onClick={this.handleClickOnHeader}>
                 <h1>this is <b>Header</b> component,below is TestClass</h1>
                 <TestClass />
             </div>
@@ -124,8 +125,61 @@ class TestClass extends Component {
     }
 }
 
+class LikeButton extends Component{
+    constructor(){
+        // the super() function is a must,because this class is one of the child class of the class Component
+        // this class hasn't its own object,it just only use the object which is inherited from its father class
+        // but the key word 'this' is refer to an object and this class hasn't its own object,if don't use super(),if will haven't object forever
+        // and you can't set Attributes to an object which is not existed.
+
+        // In a word,in the child class you wanna use key word 'this',you must use 'super()' first
+        super()
+        this.state = { isLike: false }
+    }
+
+    handleClickOnLikeButton(){
+        // setState is react's func,it writes like below:
+        // this.setState({
+        //     key1: value1,
+        //     key2: value2
+        // })
+        // also,the setState function is like setTimeOut,it will store the state into a query
+        // then the browser run other js code.In the end,it will change the state into what you set
+        console.log('The setState function hadn\'t run yet,the state is'+this.state.isLike)
+        this.setState({
+            isLike: !this.state.isLike
+        })
+        // this.setState({ count: 0 }) // => this.state.count 还是 undefined
+        // this.setState({ count: this.state.count + 1}) // => undefined + 1 = NaN
+        // this.setState({ count: this.state.count + 2}) // => NaN + 2 = NaN
+
+        // use this to add the attribute count's value
+        this.setState((preState) => {
+            return {
+                count:preState.count?preState.count:0
+            }
+        })
+        this.setState((preState) => {
+            return {
+                count:preState.count+1
+            }
+        })
+        console.log('The setState function had run ,the state is'+this.state.isLike)
+    }
+
+    render(){
+        return (
+            <button onClick={this.handleClickOnLikeButton.bind(this)}>
+                {/*{this.state.isLike?'don\'t like':'like'} this is emoji = =*/this.state.count?this.state.count:"undefined"}
+            </button>
+        )
+    }
+}
+
+
 class IndexClass extends Component{
     render(){
+
         return(
             <div>
                 <Header/>
@@ -138,7 +192,19 @@ class IndexClass extends Component{
     }
 }
 
-ReactDOM.render(<IndexClass />, document.getElementById('root'));
+class Index extends Component{
+    render(){
+        return(
+            <div>
+                <LikeButton/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<IndexClass />,document.getElementById('root'));
+
+ReactDOM.render(<Index/>,document.getElementById('root2'));
 // ReactDOM.render(<TestClass />, document.getElementById('root'));
 // ReactDOM.render(<TestClass />, document.getElementById('root1'));
 
