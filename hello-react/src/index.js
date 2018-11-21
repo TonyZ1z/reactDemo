44eb732a110e594ff52cc6bcfa9e906c5658b545
TonyZ1z <1104606022@qq.com>
2018-11-22 01:22:11 +0800

    add with an question

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -125,6 +125,44 @@ class TestClass extends Component {
     }
 }
 
+// this button class use props to set the text when use
+class LikeButtonProps extends Component{
+    constructor(){
+        super()
+        this.state = { isLike: false}
+    }
+// set the defaultProps and then you don't need to judge if the props is given or not
+    static defaultProps = {
+        wording:{
+            likeText: "it's real default likeText",
+            unlikeText: "it's real default unlikeText"
+        }
+    }
+// the value of one prop is read-only,so you can't change its value in a function like:
+// this.props.wording.likeText = "any"
+// because if it's permitted,when assign the value to a class,the result
+// may be confused
+// so if you wanna change the button's value,you should re_render this one again
+    handleClickOnlikeButon(){
+        this.setState({
+            isLike: !this.state.isLike
+        })
+        if(this.props.onClick){
+            this.props.onClick()
+        }
+    }
+
+    render(){
+        const likeText = this.props.wording.likeText
+        const unlikeText = this.props.wording.unlikeText
+        return(
+            <button onClick={this.handleClickOnlikeButon.bind(this)}>
+                {this.state.isLike?likeText:unlikeText}
+            </button>
+        )
+    }
+}
+
 class LikeButton extends Component{
     constructor(){
         // the super() function is a must,because this class is one of the child class of the class Component
@@ -176,17 +214,106 @@ class LikeButton extends Component{
     }
 }
 
+class Dog extends Component{
+    constructor(){
+        super()
+        this.state = {
+            isRunning: false,
+            isBarking:false
+        }
+    }
+    // wait for 2 seconds and then change the string
+    bark(){
+        console.log("it's barking now")
+        this.setState({
+            isBarking: true
+        })
+        setTimeout(() => this.setState({
+            isBarking: false
+        }),2000)
+    }
+
+    running(){
+        console.log("it's running now")
+        this.setState({
+            isRunning: true
+        })
+        setTimeout(()=> this.setState({
+            isRunning: false
+        }),2000)
+    }
+
+    handleClickEvent(){
+        this.running()
+        this.bark()
+    }
+
+    render(){
+        return(
+            <div onClick={this.handleClickEvent.bind(this)}>
+                DOG's state is {this.state.isRunning?"yes":"no"}
+            </div>
+        )
+    }
+}
+
+class Computer extends Component{
+    constructor(){
+        super()
+        this.state = {
+            status: "off"
+        }
+    }
+
+    handlePowerOnAndOff(){
+        this.setState({
+            status : this.state.status === "off"?"on":"off"
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.handlePowerOnAndOff.bind(this)}>the status now is {this.state.status}</button>
+                <Screen status = {this.state.status}/>
+                <Screen status = {this.state.status} showContent = "text content"/>
+            </div>
+        )
+    }
+}
+
+// it seems that once the defaultProps is an class,then it will lose its function?
+class Screen extends Component{
+    static defaultProps = {
+        showContent: "nothing here"
+    }
+
+    render(){
+        return(
+            <div className="screen">
+                {this.props.status === "off"?"the screen is off":this.props.showContent}
+            </div>
+        )
+    }
+}
+
 
 class IndexClass extends Component{
     render(){
 
         return(
             <div>
+                <LikeButtonProps wording={{likeText:"like",unlikeText:"unlike"}}
+                onClick = {()=>console.log("now you click the LikeButtonProps button")}/>
+                <LikeButtonProps/>
+                <Dog/>
                 <Header/>
+                <Index/>
                 <Content/>
                 <Footer/>
                 <TestBindDouble/>
                 <TestBindDouble2/>
+                <Computer/>
             </div>
         )
     }
@@ -204,7 +331,7 @@ class Index extends Component{
 
 ReactDOM.render(<IndexClass />,document.getElementById('root'));
 
-ReactDOM.render(<Index/>,document.getElementById('root2'));
+// ReactDOM.render(<Index/>,document.getElementById('root2'));
 // ReactDOM.render(<TestClass />, document.getElementById('root'));
 // ReactDOM.render(<TestClass />, document.getElementById('root1'));
 
